c4c95b03ab2362b6b44fc34e0ad8e612544dcaa2
Lucas Fiori <lucas.fiori@acct.global>
2023-10-24 07:35:17 -0300

    nv2-maturidade

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,21 +1,21 @@
 import { Router } from "express";
-import { createDevice } from "./routes/device/createDevice";
-import { listDevices } from "./routes/device/listDevices";
-import { deleteDevice } from "./routes/device/deleteDevice";
-import { getDevice } from "./routes/device/getDevice";
-import { updateDevice } from "./routes/device/updateDevice";
+import { createDevice } from "./routes/devices/createDevice";
+import { listDevices } from "./routes/devices/listDevices";
+import { deleteDevice } from "./routes/devices/deleteDevice";
+import { getDevice } from "./routes/devices/getDevice";
+import { updateDevice } from "./routes/devices/updateDevice";
 import { saveLocation } from "./routes/location/saveLocation";
-import { locationHistory } from "./routes/device/locationHistory";
+import { locationHistory } from "./routes/devices/locationHistory";
 
 const router = Router();
 
 // Device
-router.get("/device", listDevices);
-router.get("/device/:deviceId", getDevice);
-router.get("/device/:deviceId/location", locationHistory);
-router.post("/device", createDevice);
-router.delete("/device/:deviceId", deleteDevice);
-router.patch("/device/:deviceId", updateDevice);
+router.get("/devices", listDevices);
+router.get("/devices/:deviceId", getDevice);
+router.get("/devices/:deviceId/location", locationHistory);
+router.post("/devices", createDevice);
+router.delete("/devices/:deviceId", deleteDevice);
+router.patch("/devices/:deviceId", updateDevice);
 
 //Location
 router.post("/location", saveLocation);
